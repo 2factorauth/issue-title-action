eb91a1df27889f4c6fac60156407910d0bc648f4
Carlgo11 <carlgo11@users.noreply.github.com>
2024-04-12 00:19:44 +0200

    Add ranking and content checks

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const {Octokit} = require('@octokit/core');
 
 async function run() {
@@ -27,16 +26,88 @@ async function run() {
   const data = JSON.parse(JSON.parse(core.getInput('data')))
   const site_name = data['site-name']?.text
   const repo = decodeURIComponent(core.getInput('repository'))
+  const issue = core.getInput('issue_number')
+  const domain = data['site-url']?.text
+
+  // Check if adult website
+  if (await checkRating(domain)) {
+
+    await comment(repo, issue, core.getInput('adult_reply'))
+
+    await closeIssue(repo, issue)
+  }
+
+  // Check if above 200K
+  if (core.getInput('similarweb_key'))
+    if (await checkRank(domain) > 200_000) {
+    await comment(repo, issue, core.getInput('rank_reply'))
+    await closeIssue(repo, issue)
+  }
+
   return await octokit.request(`PATCH /repos/${repo}/issues/{issue_number}`, {
-    issue_number: core.getInput('issue_number'),
-    title: `${prefix} ${site_name}`,
-    headers: {
+    issue_number: core.getInput('issue_number'), title: `${prefix} ${site_name}`, headers: {
       'X-GitHub-Api-Version': '2022-11-28'
     }
   })
+}
+
+async function closeIssue(repo, issue) {
+  return await octokit.request(`PATCH /repos/{repo}/issues/{issue_number}`, {
+    issue_number: issue, repo: repo, headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  })
+}
+
+/**
+ * Post a comment to the issue
+ * @param repo
+ * @param issue
+ * @param text
+ * @returns {Promise<void>}
+ */
+async function comment(repo, issue, text) {
+  await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
+    owner: '2factorauth', repo: repo, issue_number: issue, body: text, headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  })
+}
+
+/**
+ * Check if a URL responds with adult content.
+ * @param url URL to check.
+ * @returns {Promise<boolean>} returns true on adult content.
+ */
+async function checkRating(url) {
+  const res = await fetch(url)
+  const html = await res.text()
+  const parser = new DOMParser()
+  const doc = parser.parseFromString(html, 'text/html')
+  const ratingMetaTags = doc.querySelectorAll('meta[name="rating"]')
+  if (!ratingMetaTags > 0) {
+    const ratings = Array.from(ratingMetaTags).map(tag => tag.getAttribute('content'));
+    return ratings.some(rating => ['adult', 'mature', 'RTA-5042-1996-1400-1577-RTA'].includes(rating));
+  }
+  return false
 }
 
 run().catch(error => {
   console.error(error);
   core.setFailed(error.message)
 });
+
+/**
+ * Fetch the SimilarWeb global ranking of a domain.
+ * @param domain domain to fetch rank for
+ * @returns {Promise<number>} Returns the SimilarWeb rank
+ */
+async function checkRank(domain) {
+  const res = await fetch(`https://api.similarweb.com/v1/similar-rank/${domain}/rank?api_key=${api_key}`)
+
+  // Return rank 999,999 on failure
+  if (res.status !== 200) return 999_999;
+
+  const data = await res.json();
+  return data['similar_rank']['rank']
+}
